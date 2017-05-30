13c2ea1176d6ee861831df390599b081843d5c9e
alex <alex@staticlibs.net>
2017-05-30 23:21:40 +0100

    test globals cleanup

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
 define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+var test = require("tape-compat");
+var describe = test.describe;
+var it = test.it;
 var assert = require("assert"),
     entities = require("entities/");
 
